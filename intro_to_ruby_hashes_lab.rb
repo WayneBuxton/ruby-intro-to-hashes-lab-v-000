a965be65777c5c91658617f700582544c4a97ffc
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-29 19:26:02 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,12 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads => {},
-		:pieces => 4,
+		:railroads => {}
 	}
-
+	railroads = {
+		:pieces => 4
+	}
+	
 end
 
 def monopoly_with_third_tier
